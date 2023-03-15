bc2116ebbf69d867ed9f31079caebe7a762c5d32
kat <kmecseki@gmail.com>
2023-03-14 20:31:38 -0700

    Adding missing files to repo

---
+++ settings.lua
@@ -0,0 +1,15 @@
+data:extend({
+	{
+		name = "number_of_units_to_check_per_update",
+		setting_type = "runtime-global",
+		type = "int-setting",
+		default_value = 50,
+        minimum_value = 1
+	},
+    {
+		name = "update_every_x_tick",
+		setting_type = "runtime-global",
+		type = "int-setting",
+		default_value = 10
+	}
+})
